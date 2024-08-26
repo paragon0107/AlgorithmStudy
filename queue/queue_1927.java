b6e8feaa1e747349e877014281364728701921bc
paragon0107 <smg0725@naver.com>
2024-08-26 20:34:48 +0900

    qw

---
+++ queue/queue_1927.java
@@ -0,0 +1,9 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class queue_1927 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+}
